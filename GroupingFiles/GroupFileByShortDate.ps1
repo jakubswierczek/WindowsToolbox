bf848f8155abc5a32337555aca1de9de5e46afec
Jakub Świerczek <jakub.swierczek@outlook.com>
2017-10-22 12:01:21 +0200

    Script for grouping file by short date

---
+++ GroupingFiles/GroupFileByShortDate.ps1
@@ -0,0 +1,6 @@
+﻿Get-ChildItem | Select Name, LastWriteTime | Group-Object { $_.LastWriteTime.ToShortDateString() } | ForEach-Object {
+    New-Item -Name $_.Name -ItemType Directory -Force
+    foreach($file in $_.Group) {
+        Move-Item $file.Name $_.Name -Force
+    }
+}
